aa43b6e472281b18688988c51a47bd6b78d92ad1
John Ferguson <aninvisiblefriend.tech@gmail.com>
2017-09-10 00:04:00 -0400

    Added more code as I finished reading blog post.

--- src/main/scala/org/sparkle/SparkSQLDive.scala
+++ src/main/scala/org/sparkle/SparkSQLDive.scala
@@ -18,5 +18,15 @@ object SparkSQLDive {
     peopleDataset.cache()
     val eldersDataset: Dataset[Person] = peopleDataset.filter(_.age.exists(_ > 65))
     eldersDataset.write.save(s"results/generated/people_filtered_${System.currentTimeMillis()}")
+
+    eldersDataset.explain(extended=true)
+
+    println("\n\n<==================================>\n\n")
+
+    eldersDataset.queryExecution.debug.codegen()
+
+    // This hack lets us go to localhost:4040 and see some RDD stuff in Spark UI
+    println("Hit Enter to Stop")
+    System.in.read()
   }
 }
